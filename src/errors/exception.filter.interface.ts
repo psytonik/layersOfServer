95b5be73cfa7416e01cd023c58864280e9e00f76
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-01-10 17:18:51 +0200

    Implementing Exception Error

---
+++ src/errors/exception.filter.interface.ts
@@ -0,0 +1,1 @@
+import {NextFunction, Request, Response} from "express";export interface ExceptionFilterInterface {    catch: (err: Error, req: Request, res: Response, next: NextFunction) => void;}
