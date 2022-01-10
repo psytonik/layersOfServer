a697242f43dca0340ef65ab5aee3de26e06ad385
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-01-10 12:39:31 +0200

    Implementing logger service

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-import  {App} from './app';const bootstrap = async ()=>{    const app = new App();    await app.init();}bootstrap()    .finally();
+import  {App} from './app';import {LoggerService} from "./logger/logger.service";const bootstrap = async ()=>{    const app = new App(new LoggerService());    await app.init();}bootstrap()    .finally();
