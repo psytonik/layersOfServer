75dd80c421386c0370a2853a0ade19430e9e63d7
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-01-12 17:37:14 +0200

    adding user entity and dto

---
+++ src/users/dto/user-registration.dto.ts
@@ -0,0 +1,5 @@
+export class UserRegistrationDto {
+	email: string;
+	password: string;
+	name: string;
+}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,23 @@
+import { hash } from 'bcryptjs';
+
+export class UserEntity {
+	constructor(private readonly _email: string, private readonly _name: string) {}
+
+	private _password: string;
+
+	get password(): string {
+		return this._password;
+	}
+
+	get email(): string {
+		return this._email;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+
+	public async setPassword(pass: string): Promise<void> {
+		this._password = await hash(pass, 10);
+	}
+}
