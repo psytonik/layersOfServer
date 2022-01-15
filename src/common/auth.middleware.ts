ec681b010e37aa722d08b4f1659f92e45c0f5a75
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-01-16 01:17:52 +0200

    adding validation middleware

---
+++ src/common/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import { MiddlewareInterface } from './middleware.interface';
+
+
+export class AuthMiddleware implements MiddlewareInterface {
+	constructor(private secret:string) {}
+
+	execute(request: Request, res: Response, next: NextFunction):void  {
+		if (request.headers.authorization) {
+			verify(request.headers.authorization.split(' ')[1],this.secret, (error,payload)=>{
+				if(error){
+					next();
+				} else if (payload){
+					request.user = payload.email;
+					next();
+				}
+			});
+		}
+		next();
+	}
+}
